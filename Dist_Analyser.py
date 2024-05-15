f050dbe9f473ffbd2e2d8e451741f741d0c9aea2
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2024-05-15 10:35:37 +0100

    Created the population_distribution_analyser module. Currently holds comments describing how the module is expected to work.

---
+++ Dist_Analyser.py
@@ -0,0 +1,13 @@
+# Gets handed a numpy array which is a single column of the population data.
+
+# Creates a Histogram of the data
+
+# Plots the theoretical log-normal distribution on the histogram
+    # Calculates the parameters
+    # Adds the plot to histogram plot
+
+# Creates a Pareto
+    # Calculate WHERE the Pareto is a better fit
+        # KS test
+    # Calculates the parameters at this point
+    # Adds the plot to histogram plot
