740e5aa0fdc0371c19b8aff385f92ece12837e21
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2024-05-15 16:58:50 +0100

    Theoretical log-normal distribution plotted. NEED TO: put all plots on the same axis (log(population)).

--- plots.py
+++ plots.py
@@ -15,26 +15,36 @@ data = pd.read_excel(r'C:\Users\lconn\Documents\Programming\Python\Python Projec
 
 # Remove zero values
 data = data.loc[(data!=0).any(axis=1)]   # axis 1 means this is applied to rows, axis 0 would be columns.
-print("------------------------------------------------------------")
 # CREATING HISTOGRAM
 # Create histogram bins
-print(data["POP"].max(axis=0))
-print("------------------------------------------------------------")
-### FIXME: correct the number of bins. 5 per order incriment?
-bins = np.logspace(start=0, stop=np.log10(data["POP"].max(axis=0)), base=10)
-midpoints = 0.5 * (bins[1:] + bins[:-1])
+### FIXME Issue 2: correct the number of bins. 5 per order incriment?
+bins = np.logspace(start=0, stop=np.log10(data["POP"].max(axis=0))+1, base=10)
 # Plot the histogram (Empirical distribution)
-fig, axs = plt.subplots(1, 2)
-axs[0].hist(data, bins=bins)
+fig, axs = plt.subplots(1, 4)
+vals, _, _ = axs[0].hist(data, bins=bins)
+# Plot midpoints
+midpoints = 0.5 * (bins[1:] + bins[:-1])
+axs[0].plot(midpoints, vals, c='green')
+axs[0].set_xscale('log')   # log scale the x axis
 
 # Theoretical Log-normal distribution
 log_data = np.log10(data)
-lognorm_mean = log_data["POP"].mean()
-lognorm_sd = log_data["POP"].std()
-print(f"lognorm mean: {lognorm_mean}, lognorm sd: {lognorm_sd}")
-x = np.linspace(lognorm_mean-3*lognorm_sd, lognorm_mean+3*lognorm_sd, 100)
-y = stats.norm.pdf(x, lognorm_mean, lognorm_sd)
-axs[1].plot(x, y)
+mu = log_data["POP"].mean()
+s = log_data["POP"].std()
+### FIXME Issue 4: x scale on plot1 goes from 0-10**5, plot2 goes from 0-5
+### Potential solution: would it be easier to also plot the historgam using log_data
+x = np.linspace(0, log_data["POP"].max()+1, 100)
+### FIXME Issue 3: y values are scaled down, wrong values.
+### Area of bin * number of samples
+y = stats.norm.pdf(x, loc=mu, scale=s)
+axs[1].plot(x, y, c='red')
+y=y*log_data["POP"].sum()  # attempt at pdf to frequency scaling
+axs[2].plot(x, y, c='red')
 
+# attempt at twinx plot
+axs2 = axs[3].twinx()
+axs[3].plot(midpoints, vals, c='orange')
+axs2.plot(x, y, c='red')
+axs2.set_box_aspect(1)
 
 plt.show()
