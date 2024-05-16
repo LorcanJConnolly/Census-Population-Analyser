6480b6588c7da9e0d9403d93367ff2d55710f514
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2024-05-16 10:14:28 +0100

    Fixed issues with theoretical log-normal distribution on the histogram plot by using log_data through the plotting process.

--- plots.py
+++ plots.py
@@ -10,41 +10,39 @@ pop = 9
 data = pd.read_excel(r'C:\Users\lconn\Documents\Programming\Python\Python Projects\Population_distribution\Data\EU_Census_2011_2016review.xlsx',
                      sheet_name='UK',
                      usecols=[pop])
-
-# print(type(data))   <class 'pandas.core.frame.DataFrame'>
-
+# ==============================================================================================================
 # Remove zero values
 data = data.loc[(data!=0).any(axis=1)]   # axis 1 means this is applied to rows, axis 0 would be columns.
+log_data = np.log10(data)
+### FIXME remove null values. 
+# ==============================================================================================================
 # CREATING HISTOGRAM
+fig, axs = plt.subplots(1, 5)
 # Create histogram bins
 ### FIXME Issue 2: correct the number of bins. 5 per order incriment?
 bins = np.logspace(start=0, stop=np.log10(data["POP"].max(axis=0))+1, base=10)
 # Plot the histogram (Empirical distribution)
-fig, axs = plt.subplots(1, 4)
 vals, _, _ = axs[0].hist(data, bins=bins)
 # Plot midpoints
+#### FIXME: plot and join midpoints.
 midpoints = 0.5 * (bins[1:] + bins[:-1])
 axs[0].plot(midpoints, vals, c='green')
 axs[0].set_xscale('log')   # log scale the x axis
-
+# ==============================================================================================================
 # Theoretical Log-normal distribution
-log_data = np.log10(data)
 mu = log_data["POP"].mean()
 s = log_data["POP"].std()
-### FIXME Issue 4: x scale on plot1 goes from 0-10**5, plot2 goes from 0-5
-### Potential solution: would it be easier to also plot the historgam using log_data
+### FIXME: variable names
 x = np.linspace(0, log_data["POP"].max()+1, 100)
-### FIXME Issue 3: y values are scaled down, wrong values.
-### Area of bin * number of samples
 y = stats.norm.pdf(x, loc=mu, scale=s)
 axs[1].plot(x, y, c='red')
-y=y*log_data["POP"].sum()  # attempt at pdf to frequency scaling
-axs[2].plot(x, y, c='red')
-
-# attempt at twinx plot
-axs2 = axs[3].twinx()
-axs[3].plot(midpoints, vals, c='orange')
-axs2.plot(x, y, c='red')
-axs2.set_box_aspect(1)
-
+# ==============================================================================================================
+# Histogram plot with log data
+### WORKS AS INTENDED ###
+### FIXME: add the counts axis to the right size of the plot. ###
+log_bins = np.arange(0, log_data["POP"].max()+1, 0.05)
+axs[4].hist(log_data, bins=log_bins)
+axs[3].hist(log_data, bins=log_bins, density=True)   # Area under the curve = 1, like a pdf.
+axs[3].plot(x, y, c='red')
+# ==============================================================================================================
 plt.show()
